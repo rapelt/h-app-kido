a9c445d895b5f99dc65e01d00ba3493547f74d87
rapelt <rebekahapelt@gmail.com>
2015-12-16 16:41:28 +1000

    added watchify to browsify and login functionality

--- public/main.js
+++ public/main.js
@@ -1,16 +1,18 @@
-// main.js
-
-
-// Bootstrap wants jQuery global =(
 window.jQuery = $ = require('jquery')
 var cssify = require('cssify')
-// Bootstrap doesn't have a "main" field / export anything =(
+
 var bootstrap = require('bootstrap/dist/js/bootstrap')
 
-require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
 
-var react = require('react');
-var reactDOM = require('react-dom');
+var angular = require('angular')
+var angularroute = require('angular-route')
+var angularcookies = require('angular-cookies')
 
-
-
+var app = require('./app.js')
+require('./app-services/authentication.service.js');
+require('./app-services/user.service.local-storage.js');
+require('./home/home.controller.js');
+require('./login/login.controller.js')
+require('./register/register.controller.js')
+require('./app-services/flash.service.js')

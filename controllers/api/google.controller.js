bfd96e5c127b053f47ac42555f006aa1a2146df9
rapelt <rebekahapelt@gmail.com>
2016-04-04 18:00:53 +1000

    stuff

--- controllers/api/google.controller.js
+++ controllers/api/google.controller.js
@@ -6,8 +6,54 @@ router.post('/googleClassAttendance', googleClassAttendance);
 
 module.exports = router;
 
+var daysArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var dates = [];
+var firstTrainingColumn = 0;
+var lastTraingingColumn = 0;
+
 function googleClassAttendance(req, res) {
-    var stuff = JSON.parse(req.body);
-    console.log(stuff[0][0]);
+    var stuff = req.body;
+    getSheetData(stuff);
+    console.log(dates);
+
     res.sendStatus(200);
+}
+
+function getSheetData(result){
+        dates = [];
+        firstTrainingColumn = _.findIndex(result[0], findDay);
+        lastTraingingColumn = _.findLastIndex(result[0], findDay);
+        constructDates(result);
+}
+
+
+function findDay(day) {
+    return _.find(daysArray, function (dayName) {
+        if (day == dayName) {
+            return true;
+        }
+    });
+}
+
+function constructDates(result){
+    _.each(result[0], function(dateName, index){
+        var date = {};
+        if(index >= firstTrainingColumn && index <= lastTraingingColumn){
+            date.dayName = dateName;
+            dates.push(date);
+        }
+    });
+
+    //date number
+    _.each(result[1], function(dateName, index){
+        if(index >= firstTrainingColumn && index <= lastTraingingColumn){
+            dates[index - firstTrainingColumn].dateNumber = dateName;
+        }
+    });
+
+    _.each(result[2], function(dateName, index){
+        if(index >= firstTrainingColumn && index <= lastTraingingColumn){
+            dates[index - firstTrainingColumn].date = dateName;
+        }
+    });
 }

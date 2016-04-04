57fe2a8a22b79a94a04059a292a5ee0eaaccf11d
rapelt <rebekahapelt@gmail.com>
2016-04-04 17:31:10 +1000

    stuff

--- controllers/api/google.controller.js
+++ controllers/api/google.controller.js
@@ -7,7 +7,7 @@ router.post('/googleClassAttendance', googleClassAttendance);
 module.exports = router;
 
 function googleClassAttendance(req, res) {
-    var stuff = req.body;
+    var stuff = JSON.parse(req.body);
     console.log(stuff[0][0]);
     res.sendStatus(200);
 }

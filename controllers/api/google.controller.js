15547f8921a9637a2f7ac54f0354c18db9ae81cd
rapelt <rebekahapelt@gmail.com>
2016-04-05 16:01:15 +1000

    stuff

--- controllers/api/google.controller.js
+++ controllers/api/google.controller.js
@@ -22,7 +22,7 @@ function googleClassAttendance(req, res) {
     studentsAttendance = [];
 
     var googleResult = req.body;
-    console.log(googleResult[googleResult.length-1]);
+    console.log(Date.now(), googleResult[googleResult.length-1]);
     getSheetData(googleResult[0]);
     res.sendStatus(200);
 }
